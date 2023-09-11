4421994a8ab8107816c25d2a785eb80b616335d6
unknown <PhamTatDung>
2023-09-11 15:28:36 +0900

    add file

--- session8/list3/bai6/index.js
+++ session8/list3/bai6/index.js
@@ -15,10 +15,10 @@ let arr = [1, 1, 1, 5, 5, 5, 4, 1, 1];
 
 ///////////////////////////////////////////////
 ////////////////////////////////////////////////
-let newArr = [];
-for (let i = 0; i < arr.length; i++) {
-    if (!newArr.includes(arr[i])) {
-        newArr.push(arr[i]);
-    }
-}
-console.log(newArr);
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//     if (!newArr.includes(arr[i])) {
+//         newArr.push(arr[i]);
+//     }
+// }
+// console.log(newArr);
