f7e72b6d6d85e3bd6aacaa166940953e707375bc
unknown <PhamTatDung>
2023-09-11 15:26:46 +0900

    add file

--- session8/list3/bai6/index.js
+++ session8/list3/bai6/index.js
@@ -1,18 +1,24 @@
 let arr = [1, 1, 1, 5, 5, 5, 4, 1, 1];
-let newArr = [arr[0]];
-for (let i = 1; i < arr.length; i++) {
-    let kiemTra = true;
-    for (let j = 0; j < newArr.length; j++) {
-        if (arr[i] == newArr[j]) {
-            kiemTra = false;
-        }
-    }
-    if (kiemTra == true) {
-        newArr.push(arr[i])
-    }
-}
-console.log(newArr);
+// let newArr = [arr[0]];
+// for (let i = 1; i < arr.length; i++) {
+//     let kiemTra = true;
+//     for (let j = 0; j < newArr.length; j++) {
+//         if (arr[i] == newArr[j]) {
+//             kiemTra = false;
+//         }
+//     }
+//     if (kiemTra == true) {
+//         newArr.push(arr[i])
+//     }
+// }
+// console.log(newArr);
 
 ///////////////////////////////////////////////
 ////////////////////////////////////////////////
-
+let newArr = [];
+for (let i = 0; i < arr.length; i++) {
+    if (!newArr.includes(arr[i])) {
+        newArr.push(arr[i]);
+    }
+}
+console.log(newArr);
