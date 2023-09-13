5a03cfcd587b481b505c0dc83d7df6599c93899c
unknown <PhamTatDung>
2023-09-13 19:55:34 +0900

    add file

--- session10/bai7/index.js
+++ session10/bai7/index.js
@@ -1,10 +1,13 @@
 let n1 = +prompt('Nhap vao so thu 1');
 let n2 = +prompt('Nhap vao so thu 2');
-function doiCho(a, b) {
-    var temp = a;
-    a = b;
-    b = temp;
-    return [a, b];
+let arr = [];
+arr.push(n1);
+arr.push(n2);
+console.log('vi tri khi chua doi cho la: ',arr);
+function doiCho2So(a) {
+    var temp = a[0];
+    a[0] = a[1]
+    a[1] = temp;
+    return arr;
 }
-console.log('thu tu truoc khi doi la: ', [n1, n2]);
-console.log('thu tu sau khi doi la: ', doiCho(n1, n2));
+console.log('thu tu sau khi doi la: ', doiCho2So(arr));
